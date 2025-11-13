521e4332df5cf2950774892a51d819b8e879a688
BlueOCN <robgonzalezmu@gmail.com>
2025-11-13 12:59:25 -0600

    Create - Challange Setup

---
+++ HotelWebApplication/src/test/java/com/blueocn/HotelWebApplication/HotelWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blueocn.HotelWebApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
