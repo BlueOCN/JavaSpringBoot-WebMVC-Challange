5cdabced978be0fdc3f0019e65f4f3f58d41a1a1
BlueOCN <robgonzalezmu@gmail.com>
2025-11-13 22:19:04 -0600

    Update - Challange 2 Solved

---
+++ HotelWebApplication/src/main/java/com/blueocn/HotelWebApplication/controller/StaffRestController.java
@@ -0,0 +1,24 @@
+package com.blueocn.HotelWebApplication.controller;
+
+import com.blueocn.HotelWebApplication.model.Staff;
+import com.blueocn.HotelWebApplication.service.StaffService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/staff")
+public class StaffRestController {
+    private final StaffService staffService;
+
+    public StaffRestController(StaffService staffService) {
+        this.staffService = staffService;
+    }
+
+    @GetMapping
+    public List<Staff> getAllStaff() {
+        return staffService.getAllStaff();
+    }
+}
